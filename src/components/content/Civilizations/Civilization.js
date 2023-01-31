1da82a423cde8660a3d0b157b55d7b9f635fe4d6
JacoLombardo <jacopo.lombardo@outlook.com>
2023-01-31 16:22:37 +0100

    2

--- src/components/content/Civilizations/Civilization.js
+++ src/components/content/Civilizations/Civilization.js
@@ -1,56 +1,62 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import './Civilizations.css'
-import FavouriteIcon from '../../../Images/Icons/favourite1.png'
-import FavouritedIcon from '../../../Images/Icons/favourited.png'
-import Info from '../../../Images/Icons/info1.png'
+import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./Civilizations.css";
+import FavouriteIcon from "../../../Images/Icons/favourite1.png";
+import FavouritedIcon from "../../../Images/Icons/favourited.png";
+import Info from "../../../Images/Icons/info1.png";
 import {
   collection,
   deleteDoc,
   doc,
   getDocs,
-  setDoc
+  setDoc,
 } from "firebase/firestore";
-import { AuthContext } from '../../../context/AuthContext';
-import { db } from '../../../config';
+import { AuthContext } from "../../../context/AuthContext";
+import { db } from "../../../config";
 
 function Civilization({ data }) {
-
   const { user } = useContext(AuthContext);
   const [favourites, setFavourites] = useState([]);
   const [changeImg, setChangeImg] = useState(false);
 
   const addFavourite = async () => {
     try {
-      await setDoc(doc(db, "Favourites", `${user.email}`, "Civilizations", `${data.id}`), {
-        name: data.name,
-        id: data.id,
-      });
-      console.log(data.name + " added to favourites!")
+      await setDoc(
+        doc(db, "Favourites", `${user.email}`, "Civilizations", `${data.id}`),
+        {
+          name: data.name,
+          id: data.id,
+        }
+      );
+      console.log(data.name + " added to favourites!");
     } catch (e) {
       console.error("Error adding document: ", e);
     }
     setChangeImg(true);
   };
-  
+
   const removeFavourite = async () => {
     try {
-      await deleteDoc(doc(db, "Favourites", `${user.email}`, "Civilizations", `${data.id}`));
-      console.log(data.name + " removed from favourites!")
+      await deleteDoc(
+        doc(db, "Favourites", `${user.email}`, "Civilizations", `${data.id}`)
+      );
+      console.log(data.name + " removed from favourites!");
     } catch (e) {
       console.error("Error adding document: ", e);
     }
     setChangeImg(false);
-  }
+  };
 
   const getFavourite = async () => {
     try {
-      const querySnapshot = await getDocs(collection(db, "Favourites", `${user.email}`, "Civilizations"));
+      const querySnapshot = await getDocs(
+        collection(db, "Favourites", `${user.email}`, "Civilizations")
+      );
       const myFav = [];
       querySnapshot.forEach((doc) => {
         myFav.push(doc.data().id);
       });
-        setFavourites(myFav);
+      setFavourites(myFav);
     } catch (error) {
       console.log("error", error);
     }
@@ -62,30 +68,79 @@ function Civilization({ data }) {
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  
+
   return (
     <>
       <div className="cardCiv">
         <img className="cardImgCiv" src={data.banner} alt={data.name}></img>
         <div className="cardBodyCiv">
           <h1 className="titleCiv">{data.name}</h1>
-          {user && 
-          (favourites.length !== 0 ? favourites.map(() => {
-            return (favourites.includes(data.id) ?
-              <Link onClick={removeFavourite} style={{ backgroundColor: "rgba(0, 0, 0, 0)" }}><img src={FavouritedIcon} alt="favourited" title="In your favourites" className="favouriteIconCiv"></img></Link>
-              :
-              (
-                !changeImg ? <Link onClick={addFavourite} style={{ backgroundColor: "rgba(0, 0, 0, 0)" }}><img src={FavouriteIcon} alt="favourite" title="Add to favourites!" className="favouriteIconCiv"></img></Link>
-                :
-                <Link onClick={removeFavourite} style={{ backgroundColor: "rgba(0, 0, 0, 0)" }}><img src={FavouritedIcon} alt="favourited" title="In your favourites" className="favouriteIconCiv"></img></Link>
-              ))
-          }) :
-              <Link onClick={addFavourite} style={{ backgroundColor: "rgba(0, 0, 0, 0)" }}><img src={FavouriteIcon} alt="favourite" title="Add to favourites!" className="favouriteIconCiv"></img></Link>)}
+          {user &&
+            (favourites.length !== 0 ? (
+              favourites.map(() => {
+                return favourites.includes(data.id) ? (
+                  <Link
+                    onClick={removeFavourite}
+                    style={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
+                  >
+                    <img
+                      src={FavouritedIcon}
+                      alt="favourited"
+                      title="In your favourites"
+                      className="favouriteIconCiv"
+                    ></img>
+                  </Link>
+                ) : !changeImg ? (
+                  <Link
+                    onClick={addFavourite}
+                    style={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
+                  >
+                    <img
+                      src={FavouriteIcon}
+                      alt="favourite"
+                      title="Add to favourites!"
+                      className="favouriteIconCiv"
+                    ></img>
+                  </Link>
+                ) : (
+                  <Link
+                    onClick={removeFavourite}
+                    style={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
+                  >
+                    <img
+                      src={FavouritedIcon}
+                      alt="favourited"
+                      title="In your favourites"
+                      className="favouriteIconCiv"
+                    ></img>
+                  </Link>
+                );
+              })
+            ) : (
+              <Link
+                onClick={addFavourite}
+                style={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
+              >
+                <img
+                  src={FavouriteIcon}
+                  alt="favourite"
+                  title="Add to favourites!"
+                  className="favouriteIconCiv"
+                ></img>
+              </Link>
+            ))}
         </div>
-        <Link to={`c-${data.id}`}><img src={Info} alt="info" title="More info" className="infoCiv"></img></Link>
+        <Link to={`c-${data.id}`}>
+          <img
+            src={Info}
+            alt="info"
+            title="More info"
+            className="infoCiv"
+          ></img>
+        </Link>
       </div>
     </>
-  )
+  );
 }
 
-export default Civilization
+export default Civilization;
--- src/components/content/Civilizations/Civilizations.js
+++ src/components/content/Civilizations/Civilizations.js
@@ -1,70 +1,72 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import Civilization from './Civilization';
-import Loader from '../.././Loader/Loader';
-import Pagination from '../.././Pagination/Pagination';
-import SearchBar from '../.././SearchBar/SearchBar';
-import '../../SearchBar/SearchBar.css';
-import NavBar from '../.././NavBar/NavBar';
+import React from "react";
+import { useState, useEffect } from "react";
+import Civilization from "./Civilization";
+import Loader from "../.././Loader/Loader";
+import Pagination from "../.././Pagination/Pagination";
+import SearchBar from "../.././SearchBar/SearchBar";
+import "../../SearchBar/SearchBar.css";
+import NavBar from "../.././NavBar/NavBar";
 
 function Civilizations() {
-
   const [searchInput, setSearchInput] = useState("");
   const [civilizations, setCivilizations] = useState([]);
   const [loading, setLoading] = useState(true);
 
-    let filteredCivilizations = civilizations
+  let filteredCivilizations = civilizations;
 
-    
-    if (searchInput.length > 0) {filteredCivilizations = 
-        civilizations.filter((civilization) => {
-            return civilization.name.toLowerCase().includes(searchInput.toLowerCase());
-        });
-    }
+  if (searchInput.length > 0) {
+    filteredCivilizations = civilizations.filter((civilization) => {
+      return civilization.name
+        .toLowerCase()
+        .includes(searchInput.toLowerCase());
+    });
+  }
 
-    const fetchCivilizations = () => {
-        const url = "https://age-of-empires-2-api.vercel.app/api/civilizations/all";
-        fetch(url)
-            .then((response) => response.json())
-            .then((result) => {
-                setCivilizations(result);
-                setLoading(false);
-            })
-            .catch((error) => {
-                console.log("error", error);
-            });
-    }
+  const fetchCivilizations = () => {
+    const url = "https://age-of-empires-2-api.vercel.app/api/civilizations/all";
+    fetch(url)
+      .then((response) => response.json())
+      .then((result) => {
+        setCivilizations(result);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  };
 
+  useEffect(() => {
+    fetchCivilizations();
+  }, []);
 
-    useEffect(() => {
-        fetchCivilizations();
-    }, []);
-
-    return (
-      <>
-        <NavBar />
-        <div className="backgroundDiv">
-        <br/>
-          <div className="searchBar" id="search-bar-civ">
-            <h1 className="discoverFont">Discover the Civilizations</h1>
-            <SearchBar setSearchInput={setSearchInput} searchInput={searchInput} />
+  return (
+    <>
+      <NavBar />
+      <div className="backgroundDiv">
+        <br />
+        <div className="searchBar" id="search-bar-civ">
+          <h1 className="discoverFont">Discover the Civilizations</h1>
+          <SearchBar
+            setSearchInput={setSearchInput}
+            searchInput={searchInput}
+          />
         </div>
-        <br/>
-            {!loading ? (
-              <Pagination
+        <br />
+        {!loading ? (
+          <Pagination
             data={filteredCivilizations}
             RenderComponent={Civilization}
             pageLimit={3}
             dataLimit={15}
           />
-            ) : (
-              <div className="loaderDiv">
-                <Loader />
-              </div>
-          )}
+        ) : (
+          <div className="loaderDiv">
+            <Loader />
           </div>
-        </>
-  )
+        )}
+      </div>
+    </>
+  );
 }
 
-export default Civilizations
+export default Civilizations;
